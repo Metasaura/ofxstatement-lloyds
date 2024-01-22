60cb62822f9301083878af87f2fd454ea954cb72
Victoria Lebedeva <victoria@lebedev.lt>
2024-01-22 20:21:39 +0200

    Parse Lloyds statement

--- src/ofxstatement_lloyds/plugin.py
+++ src/ofxstatement_lloyds/plugin.py
@@ -1,8 +1,8 @@
-from typing import Iterable
+from typing import Iterable, Optional, TextIO
 
 from ofxstatement.plugin import Plugin
 from ofxstatement.parser import StatementParser
-from ofxstatement.statement import Statement, StatementLine
+from ofxstatement.statement import Statement, StatementLine, generate_unique_transaction_id
 
 from ofxstatement.parser import CsvStatementParser
 
@@ -17,5 +17,19 @@ class LloydsPlugin(Plugin):
 
 
 class LloydsParser(CsvStatementParser):
-    mappings = {"date": 0, "memo": 4, "bank_account_to": 3}
+    mappings = {"date": 0, "memo": 4}
     date_format = "%d/%m/%Y"
+
+    def __init__(self, fin: TextIO) -> None:
+        super().__init__(fin)
+        self.uids = set()
+
+    def parse_record(self, line: list[str]) -> Optional[StatementLine]:
+        sline = super().parse_record(line)
+        sline.id = generate_unique_transaction_id(sline, self.uids)
+        return sline
+
+    def split_records(self) -> Iterable[list[str]]:
+        reader = super().split_records()
+        next(reader)  #Skip first line
+        return reader
