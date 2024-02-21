08c3e0644a8937159c505c81e8dd44d2c7e43286
Victoria Lebedeva <victoria@lebedev.lt>
2024-02-21 21:44:45 +0200

    Make Mypy happy

--- src/ofxstatement_lloyds/plugin.py
+++ src/ofxstatement_lloyds/plugin.py
@@ -1,6 +1,6 @@
 from datetime import date
 from decimal import Decimal
-from typing import Iterable, Optional, TextIO
+from typing import Iterable, Iterator, Optional, TextIO, cast
 
 from ofxstatement.plugin import Plugin
 from ofxstatement.parser import StatementParser
@@ -31,45 +31,52 @@ class LloydsPlugin(Plugin):
 class LloydsParser(CsvStatementParser):
     mappings = {"date": 0, "memo": 4}
     date_format = "%d/%m/%Y"
-    start_balance = None
-    end_balance = None
+    start_balance: Decimal | None = None
+    end_balance: str | None = None
     start_date = None
     end_date = None
 
     def __init__(self, fin: TextIO) -> None:
         super().__init__(fin)
-        self.uids = set()
+        self.uids: set[str] = set()
 
     def parse(self) -> Statement:
         stmt = super().parse()
         stmt.start_date = self.start_date
-        stmt.start_balance = Decimal(self.start_balance)
+        if self.start_balance is not None:
+            stmt.start_balance = Decimal(self.start_balance)
         stmt.end_date = self.end_date
-        stmt.end_balance = Decimal(self.end_balance)
+        if self.end_balance is not None:
+            stmt.end_balance = Decimal(self.end_balance)
         stmt.account_id = self.account_id
         return stmt
 
     def parse_record(self, line: list[str]) -> Optional[StatementLine]:
         sline = super().parse_record(line)
+        if sline is None:
+            return None
         sline.id = generate_unique_transaction_id(sline, self.uids)
         account_id = line[3]
-        debit = line[5]
-        credit = line[6]
-        balance = line[7]
+        debit_str = line[5]
+        credit_str = line[6]
+        balance_str = line[7]
         self.account_id = account_id
         self.start_date = sline.date
-        if debit == '':
-            debit = 0
+
+        if debit_str == '':
+            debit = Decimal("0")
         else:
-            debit = self.parse_decimal(debit)
-        if credit == '':
-            credit = 0
+            debit = self.parse_decimal(debit_str)
+
+        if credit_str == '':
+            credit = Decimal("0")
         else:
-            credit = self.parse_decimal(credit)
+            credit = self.parse_decimal(credit_str)
+
         sline.amount = (-debit + credit)
         
         if self.end_balance == None:
-            self.end_balance = balance
+            self.end_balance = balance_str
         
         if self.end_date == None:
             self.end_date = sline.date
@@ -95,12 +102,12 @@ class LloydsParser(CsvStatementParser):
         if trtype in typemap:
             sline.trntype = typemap[trtype]
     
-        balance = self.parse_decimal(balance)
+        balance = self.parse_decimal(balance_str)
         self.start_balance = balance + debit - credit
 
         return sline
 
     def split_records(self) -> Iterable[list[str]]:
-        reader = super().split_records()
-        next(reader)  #Skip first line
+        reader = cast(Iterator[list[str]], super().split_records())
+        next(reader)  # Skip first line
         return reader
